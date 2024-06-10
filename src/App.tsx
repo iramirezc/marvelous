b0243973c3abf678999f9106b20cf36d964da9cd
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-11 01:31:41 +0200

    feat: implement search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./components/header";
 import Loader from "./components/loader/loader";
+import SearchBar from "./components/search-bar/search-bar";
 
-const App = () => (
-  <>
-    <Header
-      favoritesCount={0}
-      onLogoClick={() => console.log("Logo clicked")}
-      onFavoritesClick={() => console.log("Favorites clicked")}
-    />
-    <Loader isLoading={true} />
-  </>
-);
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      <Header
+        favoritesCount={0}
+        onLogoClick={() => console.log("Logo clicked")}
+        onFavoritesClick={() => console.log("Favorites clicked")}
+      />
+      <Loader isLoading={isLoading} />
+      <SearchBar value={searchValue} results={0} onChange={setSearchValue} />
+    </>
+  );
+};
 
 export default App;
