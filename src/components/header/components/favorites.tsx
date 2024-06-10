54518c6df069590b7f9a15007d1a72cb681b16d9
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-11 01:32:09 +0200

    chore: minor changes

--- src/components/header/components/favorites.tsx
+++ src/components/header/components/favorites.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import FilledHeartIcon from "../../icons/filled-heart-icon";
 import "./favorites.css";
 
-type FavoritesProps = {
+type Props = {
   count: number;
   onClick: () => void;
 };
 
-const Favorites = ({ count = 0, onClick }: FavoritesProps) => (
+const Favorites = ({ count = 0, onClick }: Props) => (
   <div className="my-favorites" onClick={onClick}>
     <FilledHeartIcon />
     <span className="my-favorites--count" aria-hidden="true">
