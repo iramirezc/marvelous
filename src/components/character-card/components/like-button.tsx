10ffadca55350b25cd7875cf851fc454ecdab10d
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-11 15:23:34 +0200

    test: add character card tests

--- src/components/character-card/components/like-button.tsx
+++ src/components/character-card/components/like-button.tsx
@@ -9,18 +9,20 @@ type Props = {
 };
 
 export const LikeButton = ({ liked, size = 12, onClick }: Props) => (
-  <button
-    className="character-card__like-button"
-    onClick={(event) => {
-      event.stopPropagation();
-      onClick();
-    }}
-  >
-    {liked ? (
-      <FilledHeartIcon width={size} height={size} />
-    ) : (
-      <EmptyHeartIcon width={size} height={size} />
-    )}
-    <span className="sr-only">{liked ? "Unlike" : "Like"}</span>
-  </button>
+  <div className="character-card__like-button">
+    <span className="sr-only">{liked ? "Liked" : "Not liked"}</span>
+    <button
+      onClick={(event) => {
+        event.stopPropagation();
+        onClick();
+      }}
+    >
+      {liked ? (
+        <FilledHeartIcon width={size} height={size} />
+      ) : (
+        <EmptyHeartIcon width={size} height={size} />
+      )}
+      <span className="sr-only">{liked ? "Unlike" : "Like"}</span>
+    </button>
+  </div>
 );
