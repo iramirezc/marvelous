488b20f0b01b14e82fb58c1a9cdef70285b6cf94
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-11 18:13:47 +0200

    feat: add characters list component

---
+++ src/components/characters-list/characters-list.tsx
@@ -0,0 +1,34 @@
+import React, { ComponentProps } from "react";
+import CharacterCard from "../character-card/character-card";
+import "./characters-list.css";
+
+type Props = {
+  characters: Omit<
+    ComponentProps<typeof CharacterCard>,
+    "onClick" | "onLike"
+  >[];
+  onCharacterClick: (id: string) => void;
+  onCharacterLike: (id: string) => void;
+};
+
+const CharactersList = ({
+  characters,
+  onCharacterClick,
+  onCharacterLike
+}: Props) => (
+  <div className="characters-list">
+    {characters.map((character) => (
+      <CharacterCard
+        key={character.id}
+        id={character.id}
+        name={character.name}
+        image={character.image}
+        liked={character.liked}
+        onClick={onCharacterClick}
+        onLike={onCharacterLike}
+      />
+    ))}
+  </div>
+);
+
+export default CharactersList;
