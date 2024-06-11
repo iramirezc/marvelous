e64db7c26680eac102c2531bcc50c8f434566680
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-11 18:52:43 +0200

    feat: implement characters list

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./header";
 export { default as Loader } from "./loader";
 export { default as SearchBar } from "./search-bar";
 export { default as CharacterCard } from "./character-card";
+export { default as CharactersList } from "./characters-list";
