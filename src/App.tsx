6a41a3e0073fc11c8c8081fb4d9b37d200c7270e
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-12 19:17:39 +0200

    feat: implement app state & dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { Header, Loader, SearchBar, CharactersList } from "./components";
-import characters from "./components/characters-list/mocks/characters.json";
+import { useAppState, useAppDispatch } from "./store/hooks";
+import mockCharacters from "./components/characters-list/mocks/characters.json";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [searchValue, setSearchValue] = useState("");
+  const { loading, characters, filters } = useAppState();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
+    dispatch({ type: "SET_LOADING", payload: true });
+
     setTimeout(() => {
-      setIsLoading(false);
+      dispatch({ type: "SET_LOADING", payload: false });
+      dispatch({ type: "SET_CHARACTERS", payload: mockCharacters });
     }, 1000);
   }, []);
+
+  const handleOnSearchChange = (value: string) => {
+    dispatch({ type: "SET_SEARCH_CRITERIA", payload: value });
+  };
+
+  const favoritesCount = characters.list.filter(({ liked }) => liked).length;
+
+  const filteredCharacters = characters.list.filter(({ name }) =>
+    name.toLowerCase().includes(filters.searchCriteria.toLowerCase())
+  );
 
   return (
     <>
       <Header
-        favoritesCount={0}
+        favoritesCount={favoritesCount}
         onLogoClick={() => console.log("Logo clicked")}
         onFavoritesClick={() => console.log("Favorites clicked")}
       />
-      <Loader isLoading={isLoading} />
-      <SearchBar value={searchValue} results={0} onChange={setSearchValue} />
+      <Loader isLoading={loading} />
+      <SearchBar
+        value={filters.searchCriteria}
+        results={filteredCharacters.length}
+        onChange={handleOnSearchChange}
+      />
       <CharactersList
-        characters={characters}
+        characters={filteredCharacters}
         onCharacterClick={(id) => console.log("Character", id)}
         onCharacterLike={(id) => console.log("Character like", id)}
       />
