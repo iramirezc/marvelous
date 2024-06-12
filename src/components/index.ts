d3dcd5b4542cfe10fb901b3c7b06859bc010210a
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-12 02:19:25 +0200

    test: add tests for character detail card

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Loader } from "./loader";
 export { default as SearchBar } from "./search-bar";
 export { default as CharacterCard } from "./character-card";
 export { default as CharactersList } from "./characters-list";
+export { default as CharacterDetailCard } from "./character-detail-card";
