f60381fbfd29dee417c880337aa1f9848a87c358
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-13 20:47:30 +0200

    feat: implement api searching

--- src/pages/characters/characters-page.tsx
+++ src/pages/characters/characters-page.tsx
@@ -1,19 +1,12 @@
 import React, { useEffect } from "react";
 import { CharactersList, SearchBar } from "../../components";
-import {
-  useCharacters,
-  useFavorites,
-  useFetchCharacters,
-  useFilters,
-  useSearch
-} from "../../hooks";
+import { useCharacters, useFetchCharacters, useSearch } from "../../hooks";
 import "./characters-page.css";
 
 const CharactersPage = () => {
-  const { characters } = useCharacters();
-  const { searchCriteria, setSearchCriteria } = useSearch();
-  const { favorites, toggleLikeCharacter } = useFavorites();
-  const { filters, filterCharacters } = useFilters();
+  const { characters: firstLoad } = useCharacters();
+  const { isSearching, results, searchCriteria, onChangeSearchCriteria } =
+    useSearch();
   const { fetchCharacters } = useFetchCharacters();
 
   useEffect(() => {
@@ -21,21 +14,20 @@ const CharactersPage = () => {
     //  eslint-disable-next-line
   }, []);
 
-  const filteredCharacters = filters.onlyFavorites
-    ? filterCharacters(favorites, searchCriteria)
-    : filterCharacters(characters, searchCriteria);
+  const characters = searchCriteria ? results : firstLoad;
 
   return (
     <main className="characters-page">
       <SearchBar
         value={searchCriteria}
-        results={filteredCharacters.length}
-        onChange={setSearchCriteria}
+        results={characters.length}
+        isSearching={isSearching}
+        onChange={onChangeSearchCriteria}
       />
       <CharactersList
-        characters={filteredCharacters}
-        onCharacterClick={(id) => console.log("Character", id)}
-        onCharacterLike={toggleLikeCharacter}
+        characters={characters}
+        onCharacterClick={(id) => console.log("Character->click", id)}
+        onCharacterLike={(id) => console.log("Character->like", id)}
       />
     </main>
   );
