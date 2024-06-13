78eb265ad2417fb136c32ae990bffe1cf9758be2
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-13 17:10:31 +0200

    fix: formatting

--- src/services/api/api-service.ts
+++ src/services/api/api-service.ts
@@ -3,8 +3,10 @@ import { getPublicApiEndpoint, getApiParams } from "../../utils/api-utils";
 const CHARACTERS_LIMIT = 50;
 
 const getCharacters = async <T>(params?: { nameStartsWith: string }) => {
-  const endpoint = getPublicApiEndpoint("characters")
-  const allParams = new URLSearchParams(getApiParams({ ...params, limit: CHARACTERS_LIMIT }))
+  const endpoint = getPublicApiEndpoint("characters");
+  const allParams = new URLSearchParams(
+    getApiParams({ ...params, limit: CHARACTERS_LIMIT })
+  );
 
   const response = await fetch(new URL(`${endpoint}?${allParams}`));
   const data = await response.json();
