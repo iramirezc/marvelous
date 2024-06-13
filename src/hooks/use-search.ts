74ae617ca30c798633eadd858e9da357976d5611
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-13 20:45:32 +0200

    refactor: use search to improve ux while searching

--- src/hooks/use-search.ts
+++ src/hooks/use-search.ts
@@ -5,28 +5,25 @@ import charactersService from "../services/characters";
 import { Character } from "../types";
 import { useLoading } from "./use-loading";
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 900;
 
 export const useSearch = () => {
   const {
     search: { searchCriteria }
   } = useAppState();
   const dispatch = useAppDispatch();
-  const { loading, setLoading } = useLoading();
+  const { setLoading } = useLoading();
+  // This state is used to hide the results when the user is still typing
+  const [isSearching, setIsSearching] = useState(false);
   const [results, setResults] = useState<Character[]>([]);
 
-  const setSearchCriteria = (
-    ...args: Parameters<typeof setSearchCriteriaAction>
-  ) => {
-    dispatch(setSearchCriteriaAction(...args));
+  const onChangeSearchCriteria = (newSearchCriteria: string) => {
+    dispatch(setSearchCriteriaAction(newSearchCriteria));
+    setIsSearching(true);
     setResults([]);
   };
 
   const searchCharactersByName = async (name: string) => {
-    if (!name || loading) {
-      return;
-    }
-
     setLoading(true);
 
     try {
@@ -36,10 +33,18 @@ export const useSearch = () => {
       console.error(error);
     } finally {
       setLoading(false);
+      setIsSearching(false);
     }
   };
 
   useEffect(() => {
+    // If the search criteria is empty, don't search
+    if (!searchCriteria) {
+      setIsSearching(false);
+      return;
+    }
+
+    // Debounce the search to avoid making too many requests
     const id = setTimeout(() => {
       searchCharactersByName(searchCriteria);
     }, DEBOUNCE_DELAY);
@@ -50,7 +55,8 @@ export const useSearch = () => {
 
   return {
     results,
+    isSearching,
     searchCriteria,
-    setSearchCriteria
+    onChangeSearchCriteria
   };
 };
