1ccc253ccd6737f240fa3144bca0be02c1c6c61d
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-14 12:33:47 +0200

    refactor: favorites service

--- src/services/favorites/favorites-service.test.ts
+++ src/services/favorites/favorites-service.test.ts
@@ -1,27 +1,47 @@
-import storageService from "../storage/storage-service";
+import type { Character, Favorites } from "../../types";
+import _storageService from "../storage/storage-service";
 import favoritesService from "./favorites-service";
 
+const favoriteCharacterId = "1009610";
+const favoriteCharacter: Character = {
+  id: favoriteCharacterId,
+  name: "Spider-Man (Peter Parker)",
+  image: "http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg",
+  liked: true,
+  description:
+    "Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people.",
+  comics: []
+};
+
 describe("Favorites Service", () => {
+  let data: Favorites;
+
+  beforeEach(() => {
+    data = {
+      ids: [favoriteCharacterId],
+      entities: { [favoriteCharacterId]: favoriteCharacter }
+    };
+  });
+
   afterEach(() => {
-    storageService.clear();
+    _storageService.clear();
   });
 
   describe("favoritesService.save()", () => {
     test("saves data to storage", () => {
-      const data = ["1"];
-
       favoritesService.save(data);
 
-      const savedData = storageService.get("favorites");
-      expect(savedData).toEqual(data);
+      // assert is stored in the storage service
+      expect(_storageService.get("favorites")).toEqual(data);
     });
   });
 
   describe("favoritesService.get()", () => {
-    test("returns saved data from storage", () => {
-      const data = ["1"];
-      storageService.save("favorites", data);
+    beforeEach(() => {
+      favoritesService.save(data);
+    });
 
+    test("returns saved data from storage", () => {
       const savedData = favoritesService.get();
 
       expect(savedData).toEqual(data);
@@ -29,14 +49,15 @@ describe("Favorites Service", () => {
   });
 
   describe("favoritesService.clear()", () => {
-    test("clears saved data from storage", () => {
-      const data = ["1"];
-      storageService.save("favorites", data);
+    beforeEach(() => {
+      favoritesService.save(data);
+    });
 
+    test("clears saved data from storage", () => {
       favoritesService.clear();
 
-      const clearedData = storageService.get<string[]>("favorites");
-      expect(clearedData).toBeNull();
+      // assert is removed from the storage service
+      expect(_storageService.get<Favorites>("favorites")).toBeNull();
     });
   });
 });
