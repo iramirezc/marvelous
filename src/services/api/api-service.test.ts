3559ca3b736d3e19e937b210fc3d36c769dcfcfe
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-15 01:01:10 +0200

    feat: add service to fetch comics list

--- src/services/api/api-service.test.ts
+++ src/services/api/api-service.test.ts
@@ -1,5 +1,6 @@
 import fetchMock from "fetch-mock";
 import characterData from "../../mocks/miles-morales-data.json";
+import comicsData from "../../mocks/comics-data.json";
 import apiService from "./api-service";
 
 jest.mock("../../utils/time-utils", () => ({
@@ -10,13 +11,10 @@ const apiEndpoints = {
   getCharacters:
     "https://api.example.com/v1/public/characters?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150&limit=50",
   getCharactersByName:
-    "https://api.example.com/v1/public/characters?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150&nameStartsWith=spider&limit=50"
+    "https://api.example.com/v1/public/characters?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150&nameStartsWith=spider&limit=50",
+  getComicsByCharacterId:
+    "https://api.example.com/v1/public/characters/1009610/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150&limit=20"
 };
-
-interface TestDataType {
-  id: string;
-  name: string;
-}
 
 describe("API Service", () => {
   afterEach(() => {
@@ -24,6 +22,11 @@ describe("API Service", () => {
   });
 
   describe("apiService.getCharacters()", () => {
+    type TestCharacterDataType = {
+      id: number;
+      name: string;
+    };
+
     test("fetches characters successfully", async () => {
       fetchMock.get(apiEndpoints.getCharacters, {
         data: {
@@ -31,7 +34,8 @@ describe("API Service", () => {
         }
       });
 
-      const response = await apiService.getCharacters<TestDataType[]>();
+      const response =
+        await apiService.getCharacters<TestCharacterDataType[]>();
 
       expect(response).toHaveLength(1);
       expect(response[0].id).toBe(1016181);
@@ -46,13 +50,37 @@ describe("API Service", () => {
         }
       });
 
-      const response = await apiService.getCharacters<TestDataType[]>({
+      const response = await apiService.getCharacters<TestCharacterDataType[]>({
         nameStartsWith: "spider"
       });
 
       expect(response).toHaveLength(1);
       expect(response[0].id).toBe(1016181);
       expect(response[0].name).toBe("Spider-Man (Miles Morales)");
+      expect(fetchMock.done()).toBe(true);
+    });
+  });
+
+  describe("apiService.getComicsByCharacterId()", () => {
+    type TestComicDataType = {
+      id: number;
+      title: string;
+    };
+
+    test("fetches comics successfully", async () => {
+      fetchMock.get(apiEndpoints.getComicsByCharacterId, {
+        data: {
+          results: comicsData
+        }
+      });
+
+      const response = await apiService.getComicsByCharacterId<
+        TestComicDataType[]
+      >({ characterId: "1009610" });
+
+      expect(response).toHaveLength(20);
+      expect(response[0].id).toBe(109637);
+      expect(response[0].title).toBe("The Amazing Spider-Man (2022) #50");
       expect(fetchMock.done()).toBe(true);
     });
   });
--- src/services/api/api-service.ts
+++ src/services/api/api-service.ts
@@ -1,6 +1,7 @@
 import { getPublicApiEndpoint, getApiParams } from "../../utils/api-utils";
 
 const CHARACTERS_LIMIT = 50;
+const COMICS_LIMIT = 20;
 
 const getCharacters = async <T>(params?: { nameStartsWith: string }) => {
   const endpoint = getPublicApiEndpoint("characters");
@@ -9,11 +10,25 @@ const getCharacters = async <T>(params?: { nameStartsWith: string }) => {
   );
 
   const response = await fetch(new URL(`${endpoint}?${allParams}`));
+
   const data = await response.json();
 
   return data.data.results as T;
 };
 
-const apiService = { getCharacters };
+const getComicsByCharacterId = async <T>(params: { characterId: string }) => {
+  const endpoint = getPublicApiEndpoint(
+    `characters/${params.characterId}/comics`
+  );
+  const allParams = new URLSearchParams(getApiParams({ limit: COMICS_LIMIT }));
+
+  const response = await fetch(new URL(`${endpoint}?${allParams}`));
+
+  const data = await response.json();
+
+  return data.data.results as T;
+};
+
+const apiService = { getCharacters, getComicsByCharacterId };
 
 export default apiService;
