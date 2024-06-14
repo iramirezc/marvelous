34833365c83733ee24375893fb0beb901f8f31da
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-14 13:18:45 +0200

    refactor: favorites service tests

--- src/services/favorites/favorites-service.test.ts
+++ src/services/favorites/favorites-service.test.ts
@@ -1,26 +1,13 @@
-import type { Character, Favorites } from "../../types";
+import type { Favorites } from "../../types";
+import mockFavorites from "../../mocks/favorites.json";
 import _storageService from "../storage/storage-service";
 import favoritesService from "./favorites-service";
-
-const favoriteCharacterId = "1009610";
-const favoriteCharacter: Character = {
-  id: favoriteCharacterId,
-  name: "Spider-Man (Peter Parker)",
-  image: "http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg",
-  liked: true,
-  description:
-    "Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people.",
-  comics: []
-};
 
 describe("Favorites Service", () => {
   let data: Favorites;
 
   beforeEach(() => {
-    data = {
-      ids: [favoriteCharacterId],
-      entities: { [favoriteCharacterId]: favoriteCharacter }
-    };
+    data = mockFavorites;
   });
 
   afterEach(() => {
