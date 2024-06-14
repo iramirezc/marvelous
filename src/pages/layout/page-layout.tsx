4261126f82a61cfca5382308a8c86a2b614a1efa
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-14 14:41:34 +0200

    feat: add navigation to header

--- src/pages/layout/page-layout.tsx
+++ src/pages/layout/page-layout.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Header, Loader } from "../../components";
-import { useFavorites, useFilters, useLoading } from "../../store/hooks";
+import { useFavorites, useLoading } from "../../store/hooks";
 import "./page-layout.css";
 
 const PageLayout = () => {
+  const navigate = useNavigate();
   const { loading } = useLoading();
   const { getFavoritesCount } = useFavorites();
-  const { showOnlyFavorites } = useFilters();
 
   return (
     <div className="page-layout">
       <Header
         favoritesCount={getFavoritesCount()}
-        onLogoClick={() => showOnlyFavorites(false)}
-        onFavoritesClick={() => showOnlyFavorites(true)}
+        onLogoClick={() => navigate("/")}
+        onFavoritesClick={() => navigate("/", { state: { favorites: true } })}
       />
       <div className="loader-container">
         <Loader isLoading={loading} />
