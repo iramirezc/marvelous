06f3abb5a84396bab2c5538c1212d279f5d95159
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-15 00:15:34 +0200

    fix: character detail card layout

--- src/components/character-detail-card/character-detail-card.tsx
+++ src/components/character-detail-card/character-detail-card.tsx
@@ -20,13 +20,19 @@ const CharacterDetailCard = ({
   onLike
 }: Props) => (
   <article className="character-detail-card">
-    <img src={picture} alt={name} />
-    <div className="character-detail-card__info">
-      <header className="character-detail-card__info-header">
-        <h2>{name}</h2>
-        <LikeButton liked={liked} onClick={() => onLike(id)} size={24} />
-      </header>
-      <p className="character-detail-card__info-description">{description}</p>
+    <div className="character-detail-card__inner">
+      <img
+        className="character-detail-card__picture"
+        src={picture}
+        alt={name}
+      />
+      <div className="character-detail-card__info">
+        <header className="character-detail-card__info-header">
+          <h2>{name}</h2>
+          <LikeButton liked={liked} onClick={() => onLike(id)} size={24} />
+        </header>
+        <p className="character-detail-card__info-description">{description}</p>
+      </div>
     </div>
     <span className="character-detail-card__corner" />
   </article>
