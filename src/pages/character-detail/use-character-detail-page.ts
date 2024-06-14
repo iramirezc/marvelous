7b25dae786f51edd0daccf52b76c4f5e27b0a5d4
Isaac Ramírez <7682555+iramirezc@users.noreply.github.com>
2024-06-15 01:39:20 +0200

    feat: implement hook to fetch comics on details page

--- src/pages/character-detail/use-character-detail-page.ts
+++ src/pages/character-detail/use-character-detail-page.ts
@@ -1,10 +1,13 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import type { Character } from "../../types";
 import { useFavoriteCharacters } from "../shared/hooks";
+import { useFetchComics } from "./hooks";
 
 export const useCharacterDetailPage = () => {
   const location = useLocation();
   const { toggleLike, isFavorite } = useFavoriteCharacters();
+  const { comics, fetchComics } = useFetchComics();
 
   const character = location.state?.character as Character | undefined;
 
@@ -20,9 +23,17 @@ export const useCharacterDetailPage = () => {
     return {
       ...character,
       liked: isFavorite(character.id),
-      description: character.description || "No description available."
+      description: character.description || "No description available.",
+      comics
     };
   };
+
+  useEffect(() => {
+    if (character) {
+      fetchComics(character.id);
+    }
+    // eslint-disable-next-line
+  }, []);
 
   return {
     character: character ? decorateCharacter(character) : undefined,
